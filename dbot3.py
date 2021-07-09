7c98251ab57d4321af441c8a6ab2183c1b0ec52c
param48 <singhparam48@outlook.com>
2021-07-09 17:26:52 +0530

    changes has been made

--- dbot3.py
+++ dbot3.py
@@ -9,14 +9,18 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 SITE = StackAPI('stackoverflow')
-SITE.key = "v)UC0zDZ3RYnXd2OpNv)sA(("
-SITE.access_token = 'zpDjJVUu0yJh2dkVQ7UWyQ))'
+SITE.key = "access_key"
+SITE.access_token = 'access_token'
 users = SITE.fetch('users/{ids}/notifications', ids=[16357192])
+
+user_notify_type=[]
+for index,value in enumerate(users['items']):
+     user_notify_type.append(users['items'][index]['notification_type'])
 
 @client.event
 async def on_message(message):
     if message.content.startswith('hello'):
-        desc = '{}',format(users['items'])
+        desc = format(user_notify_type)
         embedVar = discord.Embed(title=client.user, description=desc, color=0x00ff00)
         embedVar.add_field(name="items", value='hi', inline=False)
         embedVar.add_field(name="page", value='fy', inline=False)
