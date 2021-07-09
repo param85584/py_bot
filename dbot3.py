9902e5202bd04f890890dd3c7d65bf575e2a682d
param48 <singhparam48@outlook.com>
2021-07-09 12:47:21 +0530

    new file has been uploaded

--- dbot3.py
+++ dbot3.py
@@ -8,21 +8,18 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
 SITE = StackAPI('stackoverflow')
 SITE.key = "v)UC0zDZ3RYnXd2OpNv)sA(("
 SITE.access_token = 'zpDjJVUu0yJh2dkVQ7UWyQ))'
 users = SITE.fetch('users/{ids}/notifications', ids=[16357192])
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('$greet'):
-        channel = message.channel
-        await channel.send('Say hello!')
-        def check(m):
-            return m.content == 'hello' and m.channel == channel
-        users_msg = str(users)
-        msg = await client.wait_for('message', check=check)
-        await channel.send(f'Hello {msg.users_msg}!')
+    if message.content.startswith('hello'):
+        desc = '{}',format(users['items'])
+        embedVar = discord.Embed(title=client.user, description=desc, color=0x00ff00)
+        embedVar.add_field(name="items", value='hi', inline=False)
+        embedVar.add_field(name="page", value='fy', inline=False)
+        await message.channel.send(embed=embedVar)
 
-        
-client.run('ODYwMTE4Mzc5OTY4MjAwNzA0.YN2lqg.QG2ZjeCOG1-D302VgW14e81a1kg ')
+client.run('token')
