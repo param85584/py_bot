bfccf4e7a674ff3fdd6c74bcbf3000a3a5cc93d5
amisha2016 <amishabudhiraja96@gmail.com>
2021-07-10 18:08:25 +0530

    Fetch notifications from stackoverflow using Stack Exchange API

--- fetch_notifications.py
+++ fetch_notifications.py
@@ -6,19 +6,20 @@ load_dotenv() #access the variables from .env file. It accepts the path of .env 
 
 SITE = StackAPI('stackoverflow')
 
-SITE.key=os.getenv('STACKOVERFLOW_KEY') 
+SITE.key=os.getenv('STACKOVERFLOW_KEY') #set the app key.
 SITE.access_token=os.getenv('STACKOVERFLOW_TOKEN')
-user_id=int(os.getenv('STACKOVERFLOW_USER_ID')) 
+user_id=int(os.getenv('STACKOVERFLOW_USER_ID')) #convert the string value to integer using int function 
 
-user_notifications = SITE.fetch('users/{ids}/notifications', ids=[user_id])
+user_notifications = SITE.fetch('users/{ids}/notifications', ids=[user_id]) #it will store the notifications and other information in the dictionary form.
 
-user_notify_type=[]
-user_notify_msg=[]
-a=[]
+user_notify_type=[] #initialise to store type of notification   
+user_notify_msg=[] #initalise to store notification message
 
-for index,value in enumerate(user_notifications['items']):
+#The value of key "items" of dictionary "user_notifications" is list. Each element of this list represents single notification and it's detail in  dictionary form.
 
-    if 'body' in value.keys():
+for index,value in enumerate(user_notifications['items']): #index tell the index number of list "user_notifications['items']" and value represents the notification details of that index. The "value" is in the dictionary form.
+
+    if 'body' in value.keys(): 
         user_notify_type.append(user_notifications['items'][index]['notification_type'])
         user_notify_msg.append(user_notifications['items'][index]['body'])
 
